c01ec3ce6b3f183715ae487b52a08285e753ee8d
Sebita <sebastian.e.vogel@live.com>
2020-02-21 16:37:06 -0300

    the styles have been modified

--- src/components/SixthSection.js
+++ src/components/SixthSection.js
@@ -12,14 +12,14 @@ function SixthSection() {
           <div className="Container-SixthText">
             <div className="Text-SixthSection">
               <h3>Reserva online</h3>
-              <p className="Number-SixthSection">Para evitar las engorrosas filas desarrollamos un sistema de reserva online donde los usuarios podrán realizar una reserva o pagar el servicios que desean tanto con tarjeta de crédito como en efectivo.</p>
-              <p className="TextBold-SixthSection">Los usuarios tendrán todos los detalles de la misma:</p>
+              <p>Para evitar las engorrosas filas desarrollamos un sistema de reserva online donde los usuarios podrán realizar una reserva o pagar el servicios que desean tanto con tarjeta de crédito como en efectivo.</p>
+              <p>Los usuarios tendrán todos los detalles de la misma:</p>
               <ul className="List-SixthSection">
-                    <li>Lugar de la reserva </li> 
-                    <li>Dirección del comercio</li> 
-                    <li>Servicio contratado </li> 
-                    <li>Profesional seleccionado</li> 
-                    <li>Fecha y hora de la reserva</li> 
+                    <li>- Lugar de la reserva </li> 
+                    <li>- Dirección del comercio</li> 
+                    <li>- Servicio contratado </li> 
+                    <li>- Profesional seleccionado</li> 
+                    <li>- Fecha y hora de la reserva</li> 
               </ul>
             </div>
           </div>
